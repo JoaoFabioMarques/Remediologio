da083071f589a10095393ac866f65756722e49f3
João Fábio Marques <jfmdev4@gmail.com>
2024-12-08 16:07:38 -0300

    Tela inicial feita

---
+++ app/home.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import {
+  View,
+  Image,
+  Text,
+  TextInput,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
+import { Link, Redirect } from "expo-router";
+import { useFonts } from "expo-font";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function Home() {
+  const [fontsLoaded] = useFonts({
+    "Rajdhani-SemiBold": require("@/assets/fonts/Rajdhani-SemiBold.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <Text>Loading Fonts...</Text>;
+  }
+  return (
+    <View className="flex bg-blue-300">
+      <StatusBar barStyle="dark-content" />
+      <View className="">
+        <View className="mt-10 flex-row gap-2 items-center justify-center">
+          <Ionicons
+            name="alarm-outline"
+            size={32}
+            color="black"
+            className="mt-2"
+          />
+          <Text style={styles.rajdhani} className="text-3xl mt-4">
+            Remédiológio
+          </Text>
+        </View>
+        <View className="mt-6 items-center justify-center">
+          <Text style={styles.rajdhani} className="text-3xl">
+            Olá!
+          </Text>
+          <TextInput />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  rajdhani: {
+    fontFamily: "Rajdhani-SemiBold",
+    color: "#000000",
+  },
+});
---
+++ app/index.tsx
@@ -0,0 +1,56 @@
+import { View, Text, TextInput, StatusBar, StyleSheet, Button } from "react-native";
+import { Link } from "expo-router";
+import { useFonts } from "expo-font";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { router } from "expo-router";
+
+export function Redirecionar(){
+  return(
+    router.navigate("../home")
+  );
+}
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Rajdhani-SemiBold": require("@/assets/fonts/Rajdhani-SemiBold.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <Text>Loading Fonts...</Text>;
+  }
+  return (
+    <View className="bg-blue-300 h-full items-center">
+      <StatusBar barStyle="dark-content" />
+      <View className="mt-16">
+        <Text style={styles.rajdhani} className="text-3xl">
+          Boas-vindas ao
+        </Text>
+      </View>
+      <View className="flex-row gap-2">
+        <Ionicons
+          name="alarm-outline"
+          size={32}
+          color="black"
+          className="mt-3"
+        />
+        <Text style={styles.rajdhani} className="text-3xl mt-4">
+          Remédiológio
+        </Text>
+      </View>
+      <View className="mt-16 gap-10 w-[50%]">
+        <TextInput placeholder="Usuário" className="border border-white bg-white text-black rounded-2xl"/>
+        <TextInput placeholder="Senha" className="border border-white bg-white text-black rounded-2xl"/>
+        <View className="w-[50%] mt-4 flex justify-between flex-row gap-12 rounded-2xl text-black">
+          <Button title="Entrar" onPress={Redirecionar}/>
+          <Button title="Cadastrar"/>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  rajdhani: {
+    fontFamily: "Rajdhani-SemiBold",
+    color: "#000000",
+  },
+});
